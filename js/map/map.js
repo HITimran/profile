d1ba077297f602fa2d7ecfbafafd4dea38dab2d4
Abdul Imran <Hitimran@gmail.com>
2019-02-09 17:03:57 +0100

    map addition

--- js/map/map.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-    function initMap() {
-    var location = { lat: -25.363, lng: 131.044};
-    var map = new google.maps.Map(document.getElements("map"), {
-        zoom: 4,
-        center: location
-    }); 
-    }
